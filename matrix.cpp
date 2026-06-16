1b390794ba404135e5ca949df822eb3b594d4f38
Super-Carrot <joshua.lentz12@gmail.com>
2026-06-16 15:50:23 -0400

    Wrote matmul_naive_transposed  and matmul_vec methods

--- matrix.cpp
+++ matrix.cpp
@@ -17,19 +17,29 @@ public:
         val = vector<vector<float>> (row, vector<float>(col, 0));
     }
 
-    //Transpose
-    void T(){
-        vector<vector<float>> temp (col, vector<float>(row,0));
+    //Constructor from vector
+    matrix(int row, int col, vector<float> flat){
+        this->row = row;
+        this->col = col;
+        val = vector<vector<float>> (row, vector<float>(col, 0));
         for (int i = 0; i < row; i++){
             for (int j = 0; j < col; j++){
-                temp[j][i] = val[i][j];
+                val[i][j] = flat[i*col + j];
             }
         }
-        int temp_int = row;
-        row = col;
-        col = temp_int;
-        val = temp;
     }
+
+    //Transpose
+    matrix T() const{
+        matrix temp(col, row);
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < col; j++){
+                temp.val[j][i] = val[i][j];
+            }
+        }
+        return temp;
+    }
+
 
     //Prints out the matrix
     void print(){
@@ -52,41 +62,101 @@ public:
         return output;
     }
 
-    vector<vector<float>> unflatten(int row, int col, vector<float> flat){
+    matrix unflatten(int row, int col, vector<float>& flat){
         if (row*col != flat.size()){
             throw invalid_argument("Row col values do not align with flattened vector");
         }
-        vector<vector<float>> temp(row, vector<float>(col, 0));
+        matrix temp(row, col);
         for(int i = 0; i < flat.size(); i++){
-            temp[i/col][i%col] = flat[i];
+            temp.val[i/col][i%col] = flat[i];
         }
         return temp;
     }
 
     //Reshape matrix
-    void reshape(int row, int col){
+    matrix reshape(int row, int col){
         if (row*col != row*col){
             throw invalid_argument("Row col values do not align with original matrix");
         }
         vector<float> temp = this->flatten();
-        vector<vector<float>> output = unflatten(row, col, temp);
-        this->row = row;
-        this->col = col;
-        val = output;
+        matrix output = unflatten(row, col, temp);
+        return output;
     }
 
 };
 
+namespace matrixOps {
+    //Naive matrix multiplication
+    matrix matmul_naive(const matrix& A, const matrix& B){
+        if (A.col != B.row){
+            throw invalid_argument("Matrix 1 col does not match matrix 2 row");
+        }
+
+        matrix output(A.row, B.col);
+        for (int i = 0; i < A.row; i++){
+            for (int j = 0; j < A.col; j ++){
+                for (int k = 0; k < B.col; k++){
+                    output.val[i][k] += A.val[i][j]*B.val[j][k];
+                }
+            }
+        }
+        return output;
+    }
+    
+    //Naive matrix multiplication with transposition of second matrix
+    matrix matmul_naive_transposed(const matrix& A, const matrix& B){
+        if (A.col != B.row){
+            throw invalid_argument("Matrix 1 col does not match matrix 2 row");
+        }
+
+        matrix C = B.T();
+        matrix output(A.row, B.col);
+        for (int i = 0; i < A.row; i++){
+            for (int j = 0; j < A.col; j ++){
+                for (int k = 0; k < B.col; k++){
+                    output.val[i][k] += A.val[i][j]*C.val[k][j];
+                }
+            }
+        }
+        return output;
+    }
+
+    //Vectorized matrix multiplication
+    matrix matmul_vec(matrix& A, matrix& B){
+        if (A.col != B.row){
+            throw invalid_argument("Matrix 1 col does not match matrix 2 row");
+        }
+
+        vector<float> vecA = A.flatten();
+        vector<float> vecB = B.T().flatten();
+        
+        vector<float> output(A.row*B.col);
+
+
+        for (int i = 0; i < A.row; i++){
+            for (int j = 0; j < B.col; j++){
+                for (int k = 0; k < A.col; k++){
+                    output[i*B.col + j] += vecA[i*A.col + k]*vecB[j*B.row + k]; 
+                }
+            }
+        }
+        
+        matrix mat(A.row, B.col, output);
+        return mat;
+    }
+};
+
+
 int main(){
-    matrix test(4,6);
-    int count = 0;
+    matrix test(1000,1000);
+    int count = 1;
     for (int i = 0; i < test.row; i++){
         for (int j = 0; j < test.col; j++){
             test.val[i][j] = count++;
         }
     }
-
-    test.print();
+    matrix testT = test.T();
+    matrix multed = matrixOps::matmul_vec(test, testT);
 
     return 0;
 }
